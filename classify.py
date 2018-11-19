27586462e967a7c5ca688f8e074e2c8468158d46
Bartosz Nowak <mail@bnowak.com.pl>
2018-11-19 23:59:07 +0100

    allow classifying of single image using pre-trained classifier

---
+++ classify.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+This is a script used for classifying given image.
+"""
+import skimage.data
+
+from load_data import load_image
+from numpy import reshape
+from preprocess import preprocess_single
+from skimage import color, transform
+from sklearn.externals import joblib
+from sklearn.metrics import classification_report
+
+
+# Load model from file.
+model = joblib.load('models/predict_signs_model.pkl')
+
+# Test image file location.
+image = 'test_images/1.png'
+results_location = 'test_images/results.txt'
+
+# Load and preprocess the image.
+image = preprocess_single(load_image(image))
+
+# Predict road sign.
+prediction = model.predict(image)
+print('\nLabel of predicted road sign: ', prediction[0])
+
+# Load available road signs names.
+with open(results_location) as f:
+    results = f.readlines()
+results = [x.strip() for x in results] 
+
+# Print road sign name.
+END = 62
+for i in range(END):
+    if prediction[0] == i:
+        result = results[i]
+print('Name of predicted road sign: ', result)
