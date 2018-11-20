c5358c7e52a7b1c0bc1ae73a155d5389689eb765
Bartosz Nowak <mail@bnowak.com.pl>
2018-11-20 18:17:20 +0100

    fix lint issues

--- classify.py
+++ classify.py
@@ -3,33 +3,43 @@
 """
 This is a script used for classifying given image.
 """
-from load_data import load_image
-from preprocess import preprocess_single
 from sklearn.externals import joblib
 
+from load_data import load_image
+from preprocess import preprocess_single
 
-# Load model from file.
-model = joblib.load('models/predict_signs_model_50.pkl')
+def classify():
+    """Classify given image file and return detected sign name."""
+    # Load model from file.
+    model = joblib.load(MODEL_LOCATION)
 
+    # Load and preprocess the image.
+    image = preprocess_single(load_image(IMAGE_LOCATION))
+
+    # Predict road sign.
+    prediction = model.predict(image)
+    print('\nLabel of predicted road sign: ', prediction[0])
+
+    # Load available road signs names.
+    with open(RESULTS_LOCATION) as file_name:
+        results = file_name.readlines()
+    results = [x.strip() for x in results]
+
+    # Print road sign name.
+    end = 62
+    for i in range(end):
+        if prediction[0] == i:
+            result = results[i]
+    print('Name of predicted road sign: ', result)
+
+
+# Model file location.
+MODEL_LOCATION = joblib.load('models/predict_signs_model_50.pkl')
 # Test image file location.
-image = 'test_images/1.png'
-results_location = 'test_images/results.txt'
+IMAGE_LOCATION = 'test_images/1.png'
+# Detected signs names file location.
+RESULTS_LOCATION = 'test_images/results.txt'
 
-# Load and preprocess the image.
-image = preprocess_single(load_image(image))
 
-# Predict road sign.
-prediction = model.predict(image)
-print('\nLabel of predicted road sign: ', prediction[0])
-
-# Load available road signs names.
-with open(results_location) as f:
-    results = f.readlines()
-results = [x.strip() for x in results] 
-
-# Print road sign name.
-END = 62
-for i in range(END):
-    if prediction[0] == i:
-        result = results[i]
-print('Name of predicted road sign: ', result)
+if __name__ == '__main__':
+    classify()
--- get_model.py
+++ get_model.py
@@ -3,36 +3,44 @@
 """
 This is a script used for training chosen classifier, using a given dataset.
 """
-import display_info as di
+from sklearn.externals import joblib
+from sklearn.metrics import classification_report
+
 import model as m
 
 from load_data import load_data
 from preprocess import preprocess
-from sklearn.externals import joblib
-from sklearn.metrics import classification_report
 
 
-# Load training and testing datasets.
-train_data_dir = 'BelgiumTS/Training'
-#train_data_dir = 'GTSRB/Final_Training/Images'
-X_train, X_test, y_train, y_test = load_data(train_data_dir)
-# Print number of images.
-print('Unique training labels: {0}\nTotal training images: {1}'.format(len(set(y_train)), len(X_train)))
-print('Unique test labels: {0}\nTotal test images: {1}'.format(len(set(y_test)), len(X_test)))
-# Print images and labels info
-#di.display_images_and_labels(X_train, y_train, 'hsv')
-#di.display_label_images(X_train, 32, 'hsv', y_train)
-#di.display_label_images(X_test, 32, 'hsv', y_test)
+def get_model():
+    """Load given dataset, process it and train the classifier."""
+    # Load training and testing datasets.
+    x_train, x_test, y_train, y_test = load_data(TRAIN_DATA_DIR)
 
-# Preprocess images.
-X_train = preprocess(X_train)
-X_test = preprocess(X_test)
+    # Print number of images.
+    print('Unique training labels: {0}\nTotal training images: {1}'\
+            .format(len(set(y_train)), len(x_train)))
+    print('Unique test labels: {0}\nTotal test images: {1}'\
+            .format(len(set(y_test)), len(x_test)))
 
-# Train model.
-model = m.train_model(X_train, y_train)
-print('Model parameters:\n', model)
-# Export model to file
-joblib.dump(model, 'models/predict_signs_model.pkl', compress=9)
-# Predictions
-predictions = m.predict(model, X_test)
-print('Classification report:\n', classification_report(y_test, predictions))
+    # Preprocess images.
+    x_train = preprocess(x_train)
+    x_test = preprocess(x_test)
+
+    # Train model.
+    model = m.train_model(x_train, y_train)
+    print('Model parameters:\n', model)
+    # Export model to file.
+    joblib.dump(model, 'models/predict_signs_model.pkl', compress=9)
+    # Display predictions accuracy.
+    predictions = m.predict(model, x_test)
+    print('Classification report:\n', classification_report(y_test, predictions))
+
+
+# Dataset location.
+TRAIN_DATA_DIR = 'datasets/BelgiumTS/Training'
+#TRAIN_DATA_DIR = 'GTSRB/Final_Training/Images'
+
+
+if __name__ == '__main__':
+    get_model()
